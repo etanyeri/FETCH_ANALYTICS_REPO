b9c7d7172fc97b5c5dea68e15c2a2c04ca377762
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-11-05 21:37:00 -0800

    Update tf_dimusers.sql

--- FETCH_MART_SCHEMA/tf_dimusers.sql
+++ FETCH_MART_SCHEMA/tf_dimusers.sql
@@ -1,9 +1,8 @@
 SELECT
     , u._id AS user_id
     , u.state AS user_state
-    , u.createdDate AS user_created_date
+    , u.createdDate AS created_date
     , u.lastLogin AS user_last_login
     , u.role AS user_role
-    , u.active AS user_active
-    , u.isPremium AS user_is_premium
+    , u.active AS active
 FROM {{ source('fetch_ingest', 'users') }} AS u
