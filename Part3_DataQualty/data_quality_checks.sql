1a664069ec5c5809230263371d9ea13d88905a12
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-11-07 18:04:31 -0800

    Create data_quality_checks.sql

---
+++ Part3_DataQualty/data_quality_checks.sql
@@ -0,0 +1,12 @@
+
+-- check missing data
+SELECT COUNT(*) AS missing_data
+FROM users
+WHERE createdDate IS NULL OR _id IS NULL;
+
+
+
+-- check abnormalities
+SELECT _id, total_spent
+FROM receipts
+WHERE total_spent < 0;
