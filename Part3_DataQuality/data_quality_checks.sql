f5083b544ead50d3add90250029d4f44d5a288a6
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-11-08 22:42:13 -0800

    Update data_quality_checks.sql

--- Part3_DataQuality/data_quality_checks.sql
+++ Part3_DataQuality/data_quality_checks.sql
@@ -6,6 +6,19 @@ WHERE createdDate IS NULL OR _id IS NULL;
 
 
 
+-- Check for duplicates in factReceipts table
+SELECT 
+    USER_KEY,
+    , RECEIPT_PURCHASE_DATE
+    , RECEIPT_TOTAL_SPENT
+    COUNT(*) AS duplicate_count
+FROM 
+    factReceipts
+GROUP BY ALL
+HAVING COUNT(*) > 1;
+
+
+
 -- check abnormalities
 SELECT _id, total_spent
 FROM receipts
@@ -13,6 +26,22 @@ WHERE total_spent < 0;
 
 
 
--- check distinct values of boolean
+-- check distinct values of boolean 
 SELECT DISTINCT topBrand
 FROM brands;
+
+
+-- Check for state names that are not exactly 2 characters in length
+SELECT 
+    USER_ID
+    , USER_NAME
+    , USER_STATE AS invalid_state
+FROM 
+    dimUser
+WHERE 
+    LENGTH(USER_STATE) != 2
+    OR USER_STATE IS NULL;
+
+
+
+
