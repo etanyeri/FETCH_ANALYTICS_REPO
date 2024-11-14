2e606f438ec94fe6f5d8f4b93bd0f3cb81cdd2de
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-11-13 20:23:01 -0800

    Update factReceipts.sql

--- FETCH_MART_SCHEMA/factReceipts.sql
+++ FETCH_MART_SCHEMA/factReceipts.sql
@@ -1,18 +1,18 @@
 SELECT
     generate_surrogate_key(r.receipt_id) AS RECEIPT_KEY
-    , du.USER_KEY AS USER_KEY
-    , r.receipt_id AS RECEIPT_ID
-    , r.receipt_bonus_points_earned AS RECEIPT_BONUS_POINTS_EARNED
-    , r.receipt_bonus_points_reason AS RECEIPT_BONUS_POINTS_REASON
-    , r.receipt_create_date AS RECEIPT_CREATE_DATE
-    , r.receipt_date_scanned AS RECEIPT_DATE_SCANNED
-    , r.receipt_finished_date AS RECEIPT_FINISHED_DATE
-    , r.receipt_modify_date AS RECEIPT_MODIFY_DATE
-    , r.receipt_points_awarded_date AS RECEIPT_POINTS_AWARDED_DATE
-    , r.receipt_points_earned AS RECEIPT_POINTS_EARNED
-    , r.receipt_purchase_date AS RECEIPT_PURCHASE_DATE
-    , r.receipt_status AS RECEIPT_STATUS
-    , r.receipt_total_spent AS RECEIPT_TOTAL_SPENT
-    , r.receipt_points_per_dollar_spent AS RECEIPT_POINTS_PER_DOLLAR_SPENT
+     , du.USER_KEY
+     , r.RECEIPT_ID 
+     , r.RECEIPT_BONUS_POINTS_EARNED 
+     , r.RECEIPT_BONUS_POINTS_REASON 
+     , r.RECEIPT_CREATE_DATE
+     , r.RECEIPT_DATE_SCANNED 
+     , r.RECEIPT_FINISHED_DATE 
+     , r.RECEIPT_MODIFY_DATE 
+     , r.RECEIPT_POINTS_AWARDED_DATE 
+     , r.RECEIPT_POINTS_EARNED 
+     , r.RECEIPT_PURCHASE_DATE
+     , r.RECEIPT_STATUS 
+     , r.RECEIPT_TOTAL_SPENT 
+     , r.RECEIPT_POINTS_PER_DOLLAR_SPENT 
 FROM {{ ref('tf_receipts') }} AS r
 LEFT JOIN {{ ref('dimUsers') }} AS du ON r.user_id = u.user_id
