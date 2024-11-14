70faa2b2e42fe4efcbaedb9cdb0a011632d17ba1
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-11-13 20:19:29 -0800

    Update dimBrand.sql

--- FETCH_MART_SCHEMA/dimBrand.sql
+++ FETCH_MART_SCHEMA/dimBrand.sql
@@ -1,11 +1,11 @@
 SELECT
     generate_surrogate_key(b.brand_id) AS BRAND_KEY
-    , b.brand_id AS BRAND_ID
-    , b.brand_barcode AS BRAND_BARCODE
-    , b.brand_code AS BRAND_CODE
-    , b.brand_category AS BRAND_CATEGORY
-    , b.brand_category_code AS BRAND_CATEGORY_CODE
-    , b.brand_cpg AS BRAND_CPG
-    , b.brand_top AS IS_BRAND_TOP
-    , b.brand_name AS BRAND_NAME
+    , b.BRAND_ID
+    , b.BRAND_BARCODE
+    , b.BRAND_CODE
+    , b.BRAND_CATEGORY
+    , b.BRAND_CATEGORY_CODE
+    , b.BRAND_CPG
+    , b.IS_BRAND_TOP
+    , b.BRAND_NAME
 FROM {{ ref('tf_brands') }} AS b
