8d92b46c104b57facb4a3a4a84abd7d18d09afc6
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-11-13 20:18:02 -0800

    Update tf_receipts.sql

--- FETCH_MART_SCHEMA/tf_receipts.sql
+++ FETCH_MART_SCHEMA/tf_receipts.sql
@@ -1,19 +1,20 @@
 SELECT
-    r._id AS receipt_id
-    , r.bonusPointsEarned AS receipt_bonus_points_earned
-    , r.bonusPointsEarnedReason AS receipt_bonus_points_reason
-    , r.createDate AS receipt_create_date
-    , r.dateScanned AS receipt_date_scanned
-    , r.finishedDate AS receipt_finished_date
-    , r.modifyDate AS receipt_modify_date
-    , r.pointsAwardedDate AS receipt_points_awarded_date
-    , r.pointsEarned AS receipt_points_earned
-    , r.purchaseDate AS receipt_purchase_date
-    , r.purchasedItemCount AS receipt_item_count
-    , r.rewardsReceiptItemList AS receipt_item_list
-    , r.rewardsReceiptStatus AS receipt_status
-    , r.totalSpent AS receipt_total_spent
-    , r.userId AS user_id
+     r._id AS RECEIPT_ID
+    , r.bonusPointsEarned AS RECEIPT_BONUS_POINTS_EARNED
+    , r.bonusPointsEarnedReason AS RECEIPT_BONUS_POINTS_REASON
+    , r.createDate AS RECEIPT_CREATE_DATE
+    , r.dateScanned AS RECEIPT_DATE_SCANNED
+    , r.finishedDate AS RECEIPT_FINISHED_DATE
+    , r.modifyDate AS RECEIPT_MODIFY_DATE
+    , r.pointsAwardedDate AS RECEIPT_POINTS_AWARDED_DATE
+    , r.pointsEarned AS RECEIPT_POINTS_EARNED
+    , r.purchaseDate AS RECEIPT_PURCHASE_DATE
+    , r.purchasedItemCount AS RECEIPT_ITEM_COUNT
+    , r.rewardsReceiptItemList AS RECEIPT_ITEM_LIST
+    , r.rewardsReceiptStatus AS RECEIPT_STATUS
+    , r.totalSpent AS RECEIPT_TOTAL_SPENT
+    , r.userId AS USER_ID
+    
     -- Stakeholder Request: Total points earned vs total spent
-    , (r.pointsEarned + r.bonusPointsEarned) / NULLIF(r.totalSpent, 0) AS receipt_points_per_dollar_spent   -- Prevent division by zero
-FROM {{ source('fetch_ingest', 'receipts') }} AS r
+    , (r.pointsEarned + r.bonusPointsEarned) / NULLIF(r.totalSpent, 0) AS RECEIPT_POINTS_PER_DOLLAR_SPENT  -- Prevent division by zero
+FROM {{ source('fetch_ingest', 'receipts') }} AS r;
