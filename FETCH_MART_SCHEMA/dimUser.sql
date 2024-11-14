2e14c47a903124fb80ccd7c86bb08d1babc008d1
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-11-13 20:19:59 -0800

    Update dimUser.sql

--- FETCH_MART_SCHEMA/dimUser.sql
+++ FETCH_MART_SCHEMA/dimUser.sql
@@ -1,9 +1,9 @@
 SELECT
     generate_surrogate_key('user_id') AS USER_KEY
-    , user_id AS USER_ID
-    , user_state AS USER_STATE
-    , created_date AS USER_CREATED_DATE
-    , user_last_login AS USER_LAST_LOGIN
-    , user_role AS USER_ROLE
-    , active AS IS_ACTIVE
+    , USER_ID
+    , USER_STATE
+    , USER_CREATED_DATE
+    , USER_LAST_LOGIN
+    , USER_ROLE
+    , IS_ACTIVE
 FROM {{ ref('tf_users') }}
