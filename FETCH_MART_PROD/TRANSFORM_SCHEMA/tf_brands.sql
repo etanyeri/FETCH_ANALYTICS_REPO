e4844bb32456fb0c2e09ffb20420802084132e42
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-12-19 20:33:46 -0800

    Update tf_brands.sql

--- FETCH_MART_PROD/TRANSFORM_SCHEMA/tf_brands.sql
+++ FETCH_MART_PROD/TRANSFORM_SCHEMA/tf_brands.sql
@@ -1,1 +1,11 @@
 
+SELECT
+     b._id AS BRAND_ID
+    , b.barcode AS BRAND_BARCODE
+    , b.brandCode AS BRAND_CODE
+    , b.category AS BRAND_CATEGORY
+    , b.categoryCode AS BRAND_CATEGORY_CODE
+    , b.cpg AS BRAND_CPG
+    , b.topBrand AS BRAND_TOP
+    , b.name AS BRAND_NAME
+FROM {{ source('fetch_ingest', 'brands') }} AS b;
