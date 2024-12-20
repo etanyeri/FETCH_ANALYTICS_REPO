d39d9dd3ef383eec2d85b59f934fb8a3e1c6e22f
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-12-19 20:40:00 -0800

    Delete FETCH_MART_SCHEMA directory

--- FETCH_MART_SCHEMA/dimUser.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    generate_surrogate_key('user_id') AS USER_KEY
-    , USER_ID
-    , USER_STATE
-    , USER_CREATED_DATE
-    , USER_LAST_LOGIN
-    , USER_ROLE
-    , IS_ACTIVE
-FROM {{ ref('tf_users') }}
--- FETCH_MART_SCHEMA/tf_users.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    u._id AS USER_ID
-    , u.state AS USER_STATE
-    , u.createdDate AS CREATED_DATE
-    , u.lastLogin AS USER_LAST_LOGIN
-    , u.role AS USER_ROLE
-    , u.active AS ACTIVE
-FROM {{ source('fetch_ingest', 'users') }} AS u
